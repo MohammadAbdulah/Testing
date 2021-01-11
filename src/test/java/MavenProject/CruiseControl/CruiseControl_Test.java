9934cf247dfdda26da802e578b551aeb81ff7109
unknown <mohansary98@gmail.com>
2021-01-11 04:58:25 +0200

    fourth commit

--- src/test/java/MavenProject/CruiseControl/CruiseControl_Test.java
+++ src/test/java/MavenProject/CruiseControl/CruiseControl_Test.java
@@ -5,20 +5,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CruiseControl_Test {
-	CruiseControl control = new CruiseControl.Builder().setEngine(new GMEngine()).setBrake(new GMBrake()).setShaft(new ChevyShaft()).build();
+	CruiseControl control1 = new CruiseControl.Builder().setEngine(new GMEngine()).setBrake(new GMBrake()).setShaft(new ChevyShaft()).build();
+	CruiseControl control2 = new CruiseControl.Builder().setEngine(new ChevyEngine()).setBrake(new GMBrake()).setShaft(new ChevyShaft()).build();
 	
 	@Test
 	public void getProductInfo_GMBrake() {		 
-		 assertEquals(" [ GM Brake ] ", control.attribute.getBrake().getProductInfo());
+		 assertEquals(" [ GM Brake ] ", control1.attribute.getBrake().getProductInfo());
 	}
 	
 	@Test
 	public void getProductInfo_ChevyShaft() {		 
-		 assertEquals(" [ Chevy Shaft ] ", control.attribute.getShaft().getProductInfo());
+		 assertEquals(" [ Chevy Shaft ] ", control1.attribute.getShaft().getProductInfo());
 	}
 	
 	@Test
 	public void getProductInfo_GMEngine() {		 
-		 assertEquals(" [ GM Engine ] ", control.attribute.getEngine().getProductInfo());
+		 assertEquals(" [ GM Engine ] ", control1.attribute.getEngine().getProductInfo());
+	}
+	
+	@Test
+	public void getProductInfo_ChevyEngine() {		 
+		 assertEquals(" [ Chevy Engine ] ", control2.attribute.getEngine().getProductInfo());
 	}
 }
